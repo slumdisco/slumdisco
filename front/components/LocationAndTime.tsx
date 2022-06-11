8640ef0ae0a0c26e20e93fbed8a21f392d11459e
Cho Kaewyanurak <cho.kaewya@gmail.com>
2022-06-11 16:54:39 +0200

    enable clock

--- front/components/LocationAndTime.tsx
+++ front/components/LocationAndTime.tsx
@@ -15,22 +15,21 @@ const LocationAndTime: FC = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  // return (<Flex flexBasis='30em' marginLeft='5em' direction='column'>
+  return (<Flex flexBasis='30em' marginLeft='5em' direction='column'>
 
-  //     {/* <Text align='end' fontSize='sm'>
-  //     {time.toLocaleDateString('en-US',{ weekday: 'long' })}
-  //     </Text> */}
-  //     {/* <Text align='end'  fontSize='sm'>
-  //     {time.getDay()}/{time.getUTCMonth()}/{time.getUTCFullYear().toString().replace('20','')}
-  //     </Text> */}
-  //     <Text align='end'>
-  //     {time.getHours()}:{time.getMinutes().toString().length===1 && '0'}{time.getMinutes()}:{time.getUTCSeconds()}{time.getUTCSeconds().toString().length===1 && '0'}
-  //     </Text>
-  //     <Text align='end' fontSize='xs' fontWeight='bold'>
-  //         is your local time.
-  //     </Text>
-  // </Flex>)
-  return <Text>ok</Text>;
+      {/* <Text align='end' fontSize='sm'>
+      {time.toLocaleDateString('en-US',{ weekday: 'long' })}
+      </Text> */}
+      {/* <Text align='end'  fontSize='sm'>
+      {time.getDay()}/{time.getUTCMonth()}/{time.getUTCFullYear().toString().replace('20','')}
+      </Text> */}
+      <Text align='end'>
+      {time.getHours()}:{time.getMinutes().toString().length===1 && '0'}{time.getMinutes()}:{time.getUTCSeconds()}{time.getUTCSeconds().toString().length===1 && '0'}
+      </Text>
+      <Text align='end' fontSize='xs' fontWeight='bold'>
+          is your local time.
+      </Text>
+  </Flex>)
 };
 
 export default LocationAndTime;
